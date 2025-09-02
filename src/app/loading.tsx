2e962d8c8ed9507cc5c08c6f8c697e01a8163d8c
social hive <thesociaalhive@gmail.com>
2025-09-02 07:53:15 +0000

    theres a lot of delay when switiching tabs for example going from home t

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-1 bg-primary/20 animate-pulse">
+      <div className="h-full bg-primary animate-pulse"></div>
+    </div>
+  );
+}
